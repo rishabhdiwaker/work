ab2e3c4e2c13180cd1dd1065d6b502f20fd83cef
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2023-08-03 21:43:36 +0530

    first commit

---
+++ js/customerdetails.js
@@ -0,0 +1,33 @@
+const url = "https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp";
+const accessToken = 'dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=';
+const method = "POST";
+const headers = {
+    'Authorization': `Bearer ${accessToken}`
+};
+const body = JSON.stringify({
+  "first_name": document.getElementById("first_name").value,
+  "last_name": document.getElementById("last_name").value,
+  "street": document.getElementById("street").value,
+  "address": document.getElementById("address").value,
+  "city": document.getElementById("city").value,
+  "state": document.getElementById("state").value,
+  "email": document.getElementById("email").value,
+  "phone": document.getElementById("phone").value,
+});
+
+const createCustomer = async () => {
+  const response = await fetch(url, {
+    method: method,
+    headers: headers,
+    body: body,
+  });
+
+  if (response.status === 201) {
+    // Redirect to the customer list page
+    window.location.href = "customerlist.html";
+  } else {
+    alert("Error creating customer.");
+  }
+};
+
+document.getElementById("submit").addEventListener("click", createCustomer);
--- js/customerlist.js
+++ js/customerlist.js
@@ -34,10 +34,10 @@ fetch(apiUrl, {
         <td>${customer.email}</td>
         <td>${customer.phone}</td>
         <td>
-          <button class="delete-icon">
+          <button class="delete-icon" onclick="deleteCustomer(this)">
             <i class="fa fa-trash"></i>
           </button>
-          <button class="edit-icon">
+          <button class="edit-icon" onclick="editCustomer(this)">
             <i class="fa fa-pencil"></i>
           </button>
         </td>
@@ -48,19 +48,47 @@ fetch(apiUrl, {
   .catch(error => {
     console.error('Error:', error);
   });
-// Add the delete-icon and edit-icon classes to the buttons
-let deleteButtons;
 
 
-setTimeout(()=>{deleteButtons = document.querySelectorAll(".delete-icon");
-  deleteButtons.map((btn)=>{
-    console.log(btn)
-  })
-  console.log(deleteButtons)
-},3000)
+function deleteCustomer(button) {
+  const row = button.parentElement.parentElement;
+  const dialog = confirm('Are you sure you want to delete this customer?');
+  if (dialog) {
+    row.remove();
+  }
+}
 
 
-const editButtons = document.querySelectorAll('.edit-icon');
-editButtons.forEach(button => {
-  button.classList.add('edit-icon');
-});
+
+function editCustomer(button) {
+  const row = button.parentElement.parentElement;
+  const dialog = document.createElement('dialog');
+  dialog.innerHTML = `
+    <h2>Edit Customer</h2>
+    <input type="text" name="first_name" value="${row.querySelector('td:nth-child(1)').innerHTML}">
+    <input type="text" name="last_name" value="${row.querySelector('td:nth-child(2)').innerHTML}">
+    <input type="text" name="street" value="${row.querySelector('td:nth-child(3)').innerHTML}">
+    <input type="text" name="address" value="${row.querySelector('td:nth-child(4)').innerHTML}">
+    <input type="text" name="city" value="${row.querySelector('td:nth-child(5)').innerHTML}">
+    <input type="text" name="state" value="${row.querySelector('td:nth-child(6)').innerHTML}">
+    <input type="text" name="email" value="${row.querySelector('td:nth-child(7)').innerHTML}">
+    <input type="text" name="phone" value="${row.querySelector('td:nth-child(8)').innerHTML}">
+    <button type="submit">Save</button>
+  `;
+  dialog.classList.add('edit-dialog');
+  dialog.show();
+
+  dialog.querySelector('button').addEventListener('click', function() {
+    const firstName = dialog.querySelector('input[name="first_name"]').value;
+    const lastName = dialog.querySelector('input[name="last_name"]').value;
+    const street = dialog.querySelector('input[name="street"]').value;
+    const address = dialog.querySelector('input[name="address"]').value;
+    const city = dialog.querySelector('input[name="city"]').value;
+    const state = dialog.querySelector('input[name="state"]').value;
+    const email = dialog.querySelector('input[name="email"]').value;
+    const phone = dialog.querySelector('input[name="phone"]').value;
+
+    // Refresh the page
+    location.reload();
+  });
+}
